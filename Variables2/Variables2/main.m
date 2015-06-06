6cc080b0a79512ff120433d11e34eafd5c9fdf5c
Jackie Meggesto <jackiemeggesto@C4Qs-MacBook-Pro-13.local>
2015-06-06 11:02:25 -0400

    First use of GitX!

---
+++ Variables2/Variables2/main.m
@@ -0,0 +1,27 @@
+//
+//  main.m
+//  Variables2
+//
+//  Created by Jackie Meggesto on 6/4/15.
+//  Copyright (c) 2015 Jackie Meggesto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    
+    NSString *topLine = @"+------------------------------------------+";
+    NSString *bottomLine = @"+------------------------------------------+";
+    NSString *periodArray[] = {@"|A|", @"|B|", @"|C|", @"|D|"};
+    NSString *classArray[] = {@"Integrity Protocol", @"Liptogrammic Maths", @"Cat Fancy", @"Fomrat"};
+    NSString *teacherArray[] = {@"Mr. Gibbs", @"Mrs. Gobbs", @"Dr. Gubbs", @"Ms. Gebbs"};
+    NSLog(@"%@", topLine);
+    NSLog(@"%@ %@ %@", periodArray[0], classArray[0], teacherArray[0]);
+    NSLog(@"%@ %@ %@", periodArray[1], classArray[1], teacherArray[1]);
+    NSLog(@"%@ %@ %@", periodArray[2], classArray[2], teacherArray[2]);
+    NSLog(@"%@ %@ %@", periodArray[3], classArray[3], teacherArray[3]);
+    NSLog(@"%@", bottomLine);
+    
+    
+    return 0;
+}
---
+++ Variables2/twelveDaysOfChristmas/twelveDaysOfChristmas/main.m
@@ -0,0 +1,29 @@
+//
+//  main.m
+//  twelveDaysOfChristmas
+//
+//  Created by Jackie Meggesto on 6/4/15.
+//  Copyright (c) 2015 Jackie Meggesto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    
+//      NSString *daysOfChristmas[] = {@"First", @"Second", @"Third", @"Fourth", @"Fifth", @"Sixth", @"Seventh", @"Eighth", @"Ninth", @"Tenth", @"Eleventh", @"Twelfth"};
+//    NSString *thingsMyTrueLoveGaveToMe[] = {@"a partridge in a pear tree", @"two turtledoves", @"three French hens", @"four calling birds", @"FIVE GOLD RINGS", @"six geese a-laying", @"seven swans a-swimming", @"eight maids a-milking", @"nine ladies dancing", @"ten lords a-leaping", @"eleven pipers piping", @"twelve drummers drumming"};
+//    NSString *lotsOfGifts = {@"And"};
+//    for (int i = 0; i < 12; i++) {
+//        NSLog(@"On the %@ Day of Christmas my true love gave to me: %@" , daysOfChristmas[i], thingsMyTrueLoveGaveToMe[i]);
+//        NSLog(@"%@", lotsOfGifts);
+//        if (i == 0) {
+//            lotsOfGifts = [lotsOfGifts stringByAppendingString:thingsMyTrueLoveGaveToMe[i]];
+//        } else {
+//            lotsOfGifts = [thingsMyTrueLoveGaveToMe[i] stringByAppendingString:lotsOfGifts];
+//        }
+//    }
+    
+    
+
+    return 0;
+}
